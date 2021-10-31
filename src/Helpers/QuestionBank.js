9f26b1b67b1c3c4fe1b552cd2964261b7e585f4d
AravindBhaddriraju <aravindbhaddriraju@gmail.com>
2021-10-31 20:25:50 +0530

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,27 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3?",
+        optionA: "24",
+        optionB:"21",
+        optionC:"4",
+        optionD:"3",
+        answer: "C",
+    },
+    {
+        prompt: "Which is a streaming platform?",
+        optionA: "Twitter",
+        optionB:"NetFlix",
+        optionC:"Google",
+        optionD:"Robinghood",
+        answer: "B",
+    },
+    {
+        prompt: "What is the Name of Prime Minsiter of India",
+        optionA: "Narendra Modi",
+        optionB:"Indira Gandhi",
+        optionC:"Rahul Gandhi",
+        optionD:"Sachin Tendulkar",
+        answer: "A",
+    },
+];
+export default Questions;
